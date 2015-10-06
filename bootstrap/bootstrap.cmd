6ec8b4ec4d4b8142edac48fd2db7f5aa825dd571
Alexander Marinov <alek.marinov@intelibo.com>
2015-10-06 14:00:36 +0300

    path fix

--- bootstrap/bootstrap.cmd
+++ bootstrap/bootstrap.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 SET GIT_BIN=C:\Program Files (x86)\Git\bin
-SET MINGW64_DIR=C:\TDM-GCC-64
+SET MINGW64_DIR=C:\TDM-GCC-32
 SET MSYS_DIR=%MINGW64_DIR%\msys\1.0
 SET MINGW_BIN=%MINGW64_DIR%\bin
 SET MINGW_LIB=%MINGW64_DIR%\lib
@@ -10,7 +10,7 @@ for %%F in ("%COMSPEC%") do set SYS32DIR=%%~dpF
 
 SET PATH=%MINGW_BIN%;%MSYS_BIN%;%GIT_BIN%;%SYS32DIR%
 
-if not exist %MSYS_BIN%\which.exe copy %~dp0tools-w32\which.exe %MSYS_BIN%
+if not exist %MSYS_BIN%\which.exe copy %~dp0w32\tools\which.exe %MSYS_BIN%
 if not exist %MSYS_BIN%\sh.exe mingw-get install sh
 if not exist %MSYS_BIN%\readlink.exe mingw-get install msys-coreutils
 if not exist %MSYS_BIN%\unzip.exe mingw-get install msys-unzip
