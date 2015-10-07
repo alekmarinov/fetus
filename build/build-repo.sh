5fc88d8b8ba45ca2a12997dc44db8e0f4ed4bce0
Alexander Marinov <alek.marinov@intelibo.com>
2015-10-07 17:31:15 +0300

    copy bootstrap.sh to /los/bootstrap

--- build/build-repo.sh
+++ build/build-repo.sh
@@ -65,5 +65,5 @@ BOOTSTRAP_LOCAL_DIR=$(root_dir)/bootstrap
 
 # copy los bootstrap files
 mkdir -p "$REPO_BOOTSTRAP_DIR"
+cp -vf --remove-destination "$BOOTSTRAP_LOCAL_DIR/bootstrap.sh" "$REPO_BOOTSTRAP_DIR"
 cd $BOOTSTRAP_LOCAL_DIR && zip -r -q "$REPO_BOOTSTRAP_DIR/bootstrap-$(app_version $(root_dir)).zip" *
-# cp -vrf "$BOOTSTRAP_LOCAL_DIR/"* "$REPO_BOOTSTRAP_DIR"
