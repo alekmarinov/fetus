1c8beea9432d02e019344b54ca20b367526e3779
Alexander Marinov <alek.marinov@intelibo.com>
2015-10-07 17:34:52 +0300

    get luarocks configure patch from linux subdir

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -302,8 +302,8 @@ else
 
 	# fix luarocks configure issue in case LUA_DIR is found in / which makes invaild paths like $LUA_DIR/include -> //include
 	info "patching $LOS_ROOT/$LUAROCKS_NAME/configure"
-	download_file "$URL_REPO_BOOTSTRAP/$LUAROCKS_CONFIGURE_DIFF" "$LOS_ROOT/$LUAROCKS_NAME/$LUAROCKS_CONFIGURE_DIFF"
-	check_status "downloading $URL_REPO_BOOTSTRAP/$LUAROCKS_CONFIGURE_DIFF"
+	download_file "$URL_REPO_BOOTSTRAP/linux/$LUAROCKS_CONFIGURE_DIFF" "$LOS_ROOT/$LUAROCKS_NAME/$LUAROCKS_CONFIGURE_DIFF"
+	check_status "downloading $URL_REPO_BOOTSTRAP/linux/$LUAROCKS_CONFIGURE_DIFF"
 	patch $LOS_ROOT/$LUAROCKS_NAME/configure $LOS_ROOT/$LUAROCKS_NAME/$LUAROCKS_CONFIGURE_DIFF
 fi
 rm -f $LOS_ROOT/$LUAROCKS_PACKAGE
