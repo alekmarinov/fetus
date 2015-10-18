a2087a9483613bb5ad0ed71b64ba5a061f623f0b
Alexander Marinov <alek.marinov@intelibo.com>
2015-10-18 21:33:08 +0300

    fix build-batch-all.sh

--- build/build-batch-all.sh
+++ build/build-batch-all.sh
@@ -9,16 +9,13 @@
 ##																##
 ##################################################################
 
-LOS=los.bat
-
-DIR_INSTALL=$($LOS --config-get dir.install)
-
-# $LOS list
-cat list.test | while read name
+DIR_INSTALL=$(los --config-get dir.install)
+mkdir -p $DIR_INSTALL
+los list | while read name
 do
 	dir_install=$DIR_INSTALL/$name
-	echo -n "Regenerating $dir_install..."
-	$LOS -Ddir.install=$dir_install install $name > $DIR_INSTALL/$name.log 2>&1
+	echo -n "Regenerating $name..."
+	los -Ddir.install=$dir_install install $name > $DIR_INSTALL/$name.log 2>&1
 	if [[ $? == 0 ]]; then
 		echo "OK"
 	else
